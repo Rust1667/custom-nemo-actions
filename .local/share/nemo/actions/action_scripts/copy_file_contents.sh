578a60234cafeb0bbc29a74c46a6096961ae9b1d
Rust1667 <topphotoblog_byddwn@aleeas.com>
2024-03-31 21:42:53 +0200

    Delete copy_file_contents.sh

--- .local/share/nemo/actions/action_scripts/copy_file_contents.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exec=sh -c 'xclip -selection clipboard < %F'
-# Exec=<action_scripts/copy_file_contents.sh %F>
-
-# copy file contents to clipboard
-xclip -selection clipboard < "$1"
